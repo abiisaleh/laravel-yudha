66cf115557f54c34736227cafa54110f270693e7
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2024-02-01 00:31:26 +0900

    Update ListPerbaikanStatus.php
    add html element

--- app/Livewire/ListPerbaikanStatus.php
+++ app/Livewire/ListPerbaikanStatus.php
@@ -27,6 +27,7 @@ use IbrahimBougaoua\FilamentRatingStar\Actions\RatingStar;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\HtmlString;
 use Livewire\Component;
 
 class ListPerbaikanStatus extends Component implements HasForms, HasTable
@@ -69,10 +70,12 @@ class ListPerbaikanStatus extends Component implements HasForms, HasTable
                                     ->content(function (Perbaikan $record) {
                                         $html = '';
                                         foreach ($record->detail as $detail) {
-                                            $html .= $detail->qty . "x $detail->perbaikan ($detail->harga) = $detail->total<br>";
+$harga = number_format($detail->harga);
+$total = number_format($detail->total);
+                                            $html .= $detail->qty . "x $detail->perbaikan (Rp. $harga) = Rp. $total<br>";
                                         }
 
-                                        return $html;
+                                        return new HtmlString($html);
                                     }),
                             ]),
                         Grid::make()
